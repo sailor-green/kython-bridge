376c529454cafafa826aebe91b9127046032cb14
Nils T <nilsntth@gmail.com>
2020-04-15 16:44:36 +0200

    Attempt to make kotlin-bridge ready for MPP support

--- build.gradle.kts
+++ build.gradle.kts
@@ -1,44 +1,60 @@
 plugins {
-    kotlin("multiplatform") version "1.3.70-eap-184"
+    kotlin("multiplatform") version "1.3.71"
 }
 
 group = "green.sailor.kython"
 version = "1.0-SNAPSHOT"
 
 repositories {
-    maven { setUrl("https://dl.bintray.com/kotlin/kotlin-eap") }
     mavenCentral()
     mavenLocal()
 }
 
+val mingwPath = File(System.getenv("MINGW64_DIR") ?: "C:/msys64/mingw64")
+
 kotlin {
 
-    linuxX64 {
+    // Determine host preset.
+    val hostOs = System.getProperty("os.name")
 
-        compilations.getByName("main") {
-            sourceSets.getByName("linuxX64Main") {
-                dependencies {
-                    implementation("com.squareup.okio:okio-multiplatform:2.5.1-SNAPSHOT")
-                }
-            }
+    // Create target for the host platform.
+    val hostTarget = when {
+        hostOs == "Mac OS X" -> macosX64("cpython")
+        hostOs == "Linux" -> linuxX64("cpython")
+        hostOs.startsWith("Windows") -> mingwX64("cpython")
+        else -> throw GradleException("Host OS '$hostOs' is not a supported python bridge host.")
+    }
 
-            cinterops {
-                val cpython by cinterops.creating {
-                    defFile(project.file("cpython.def"))
-                    packageName("cpython")
-                    compilerOpts("-I/usr/include/python3.9")
-                }
+    hostTarget.apply {
+        binaries {
+            sharedLib {
+                baseName = "kython-bridge"
+                // Not sure what to put here yet for platforms other than linux.
+                linkerOpts.addAll(
+                    ("-L/usr/lib -L/usr/lib/python3.9/config-3.9-x86_64-linux-gnu/ " +
+                        "-lcrypt -lpthread -ldl -lutil -lm -lm").split(" ")
+                )
             }
         }
 
-        binaries {
-            // executable()
-            sharedLib {
-                baseName = "kython-bridge"
-                linkerOpts.addAll(
-                    ("-L/usr/lib -L/usr/lib/python3.9/config-3.9-x86_64-linux-gnu/ " +
-                    "-lcrypt -lpthread -ldl -lutil -lm -lm").split(" ")
-                )
+        compilations.getByName("main") {
+            cinterops {
+                val cpython by creating {
+                    when (preset) {
+                        presets["macosX64"] -> includeDirs.headerFilterOnly(
+                            "/usr/local/include"
+                        )
+                        presets["linuxX64"] -> includeDirs.headerFilterOnly(
+                            "/usr/include",
+                            "/usr/include/x86_64-linux-gnu"
+                        )
+                        presets["mingwX64"] -> includeDirs.headerFilterOnly(mingwPath.resolve("include"))
+                    }
+                }
+            }
+
+            dependencies {
+                implementation("com.squareup.okio:okio-multiplatform:2.5.0")
             }
         }
     }
